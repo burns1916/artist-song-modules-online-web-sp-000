38ce2000e15014f96fc365677a63c3c36e860215
Ryan K Burns <ryankburns@gmail.com>
2020-10-13 22:49:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Artist
 
   extend Memorable::ClassMethods
-  include Findable::InstanceMethods
+  include Findable::ClassMethods
   include Paramable::InstanceMethods
 
   attr_accessor :name
